00b8dc11162216a909996430a4fc9ea5c0e4406f
WySy <161842255+WannandyS@users.noreply.github.com>
2025-05-20 15:07:13 +0700

    Add files via upload

--- src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -1,15 +1,7 @@
 package com.mycompany.mavenproject3;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
 
 public class Mavenproject3 extends JFrame implements Runnable {
     private String text;
@@ -17,7 +9,6 @@ public class Mavenproject3 extends JFrame implements Runnable {
     private int width;
     private BannerPanel bannerPanel;
     private JButton addProductButton;
-    private JButton sellProductButton;
     private JTextField codeField;
     private JTextField nameField;
     private JComboBox<String> categoryField;
@@ -36,21 +27,41 @@ public class Mavenproject3 extends JFrame implements Runnable {
         bannerPanel = new BannerPanel();
         add(bannerPanel, BorderLayout.CENTER);
 
-        // Tombol "Kelola Produk" dan "Form Penjualan"
+        // Tombol "Kelola Produk"
         JPanel bottomPanel = new JPanel();
         addProductButton = new JButton("Kelola Produk");
-        sellProductButton = new JButton("Form Penjualan");
         bottomPanel.add(addProductButton);
-        bottomPanel.add(sellProductButton);
         add(bottomPanel, BorderLayout.SOUTH);
+
+        JPanel buttonPanel = new JPanel();
+        JButton addButton = new JButton("Tambah Produk");
+        JButton editButton = new JButton("Edit Produk");
+        JButton deleteButton = new JButton("Hapus Produk");
         
         addProductButton.addActionListener(e -> {
             new ProductForm().setVisible(true);
         });
 
-        sellProductButton.addActionListener(e -> {
-            new ProductSell().setVisible(true);
-        });
+        JPanel inputPanel = new JPanel(new FlowLayout());
+        codeField = new JTextField(5);
+        nameField = new JTextField(15);
+        categoryField = new JComboBox<>();
+        priceField = new JTextField(10);
+        stockField = new JTextField(5);
+        inputPanel.add(new JLabel("Kode: "));
+        inputPanel.add(codeField);
+        inputPanel.add(new JLabel("Nama: "));
+        inputPanel.add(nameField);
+        inputPanel.add(new JLabel("Kategori: "));
+        inputPanel.add(categoryField);
+        inputPanel.add(new JLabel("Harga: "));
+        inputPanel.add(priceField );
+        inputPanel.add(new JLabel("Stok: "));
+        inputPanel.add(stockField);
+
+        buttonPanel.add(addButton);
+        buttonPanel.add(editButton);
+        buttonPanel.add(deleteButton);
 
         setVisible(true);
 
